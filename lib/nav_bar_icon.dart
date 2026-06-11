ad0d794d61541d9815b8b1cb55adc33dda7f5d25
Ina Vivdici <41297525+InagoesIT@users.noreply.github.com>
2026-06-11 12:06:11 +0300

    add animation when tab changes

--- lib/nav_bar_icon.dart
+++ lib/nav_bar_icon.dart
@@ -3,39 +3,44 @@ import 'package:flutter_svg/svg.dart';
 
 class NavBarIcon extends StatelessWidget {
   final String assetName;
-  final bool isActive;
+  final bool isSelected;
+  final void Function() onTap;
 
-  const NavBarIcon({super.key, required this.assetName, this.isActive = false});
+  const NavBarIcon({
+    super.key,
+    required this.assetName,
+    required this.onTap,
+    this.isSelected = false,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return
-    // Transform.translate(
-    //   offset: Offset(0, isActive ? -5 : 0),
-    //   child:
-    SizedBox(
-      child: Column(
-        children: [
-          SvgPicture.asset(
-            assetName,
-            width: 24,
-            height: 24,
-            colorFilter: ColorFilter.mode(
-              isActive ? const Color(0xFFD4A853) : const Color(0xFF616662),
-              BlendMode.srcIn,
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12),
+        child: Column(
+          children: [
+            SvgPicture.asset(
+              assetName,
+              width: 24,
+              height: 24,
+              colorFilter: ColorFilter.mode(
+                isSelected ? const Color(0xFFD4A853) : const Color(0xFF616662),
+                BlendMode.srcIn,
+              ),
             ),
-          ),
-          Text(
-            assetName.split('/').last.split('.').first,
-            style: TextStyle(
-              fontSize: 12,
-              color: isActive
-                  ? const Color(0xFFD4A853)
-                  : const Color(0xFF616662),
+            Text(
+              assetName.split('/').last.split('.').first,
+              style: TextStyle(
+                fontSize: 12,
+                color: isSelected
+                    ? const Color(0xFFD4A853)
+                    : const Color(0xFF616662),
+              ),
             ),
-          ),
-        ],
-        // ),
+          ],
+        ),
       ),
     );
   }
--- lib/navigation_bar.dart
+++ lib/navigation_bar.dart
@@ -9,11 +9,13 @@ class CustomNavigationBar extends StatefulWidget {
   State<CustomNavigationBar> createState() => _CustomNavigationBarState();
 }
 
-class _CustomNavigationBarState extends State<CustomNavigationBar> {
-  double _bulgeCenterX = 0;
+class _CustomNavigationBarState extends State<CustomNavigationBar>
+    with SingleTickerProviderStateMixin {
   int _selectedIndex = 1;
   final List<GlobalKey> _iconKeys = List.generate(3, (_) => GlobalKey());
   final GlobalKey _barKey = GlobalKey();
+  late AnimationController _animationController;
+  late Animation<double> _bulgeAnimation;
 
   @override
   void initState() {
@@ -21,19 +23,40 @@ class _CustomNavigationBarState extends State<CustomNavigationBar> {
     WidgetsBinding.instance.addPostFrameCallback((_) {
       _updateBulgePosition(_selectedIndex);
     });
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 180),
+    );
+    _bulgeAnimation = Tween<double>(begin: 0, end: 0).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.fastOutSlowIn,
+      ),
+    );
   }
 
   void _updateBulgePosition(int index) {
     final barBox = _barKey.currentContext?.findRenderObject() as RenderBox?;
     final iconBox =
-        _iconKeys[_selectedIndex].currentContext?.findRenderObject()
-            as RenderBox?;
+        _iconKeys[index].currentContext?.findRenderObject() as RenderBox?;
     if (barBox == null || iconBox == null) return;
 
     final iconPosition = iconBox.localToGlobal(Offset.zero, ancestor: barBox);
-    final bulgeCenterX = iconPosition.dx + iconBox.size.width / 2;
+    final newBulgeCenterX = iconPosition.dx + iconBox.size.width / 2;
+
+    _bulgeAnimation =
+        Tween<double>(
+          begin: _bulgeAnimation.value,
+          end: newBulgeCenterX,
+        ).animate(
+          CurvedAnimation(
+            parent: _animationController,
+            curve: Curves.easeInOut,
+          ),
+        );
+    _animationController.forward(from: 0);
+
     setState(() {
-      _bulgeCenterX = bulgeCenterX;
       _selectedIndex = index;
     });
   }
@@ -43,29 +66,35 @@ class _CustomNavigationBarState extends State<CustomNavigationBar> {
     return SizedBox(
       key: _barKey,
       height: 90,
-      child: CustomPaint(
-        painter: NavBarPainter(_bulgeCenterX),
-        child: Padding(
-          padding: const EdgeInsets.only(top: 24),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              NavBarIcon(
-                key: _iconKeys[0],
-                assetName: 'assets/icons/quill.svg',
-                isActive: _selectedIndex == 0,
-              ),
-              NavBarIcon(
-                key: _iconKeys[1],
-                assetName: 'assets/icons/tome.svg',
-                isActive: _selectedIndex == 1,
-              ),
-              NavBarIcon(
-                key: _iconKeys[2],
-                assetName: 'assets/icons/profile.svg',
-                isActive: _selectedIndex == 2,
-              ),
-            ],
+      child: AnimatedBuilder(
+        animation: _bulgeAnimation,
+        builder: (context, _) => CustomPaint(
+          painter: NavBarPainter(_bulgeAnimation.value),
+          child: Padding(
+            padding: const EdgeInsets.only(top: 24),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                NavBarIcon(
+                  key: _iconKeys[0],
+                  assetName: 'assets/icons/quill.svg',
+                  isSelected: _selectedIndex == 0,
+                  onTap: () => _updateBulgePosition(0),
+                ),
+                NavBarIcon(
+                  key: _iconKeys[1],
+                  assetName: 'assets/icons/tome.svg',
+                  isSelected: _selectedIndex == 1,
+                  onTap: () => _updateBulgePosition(1),
+                ),
+                NavBarIcon(
+                  key: _iconKeys[2],
+                  assetName: 'assets/icons/profile.svg',
+                  isSelected: _selectedIndex == 2,
+                  onTap: () => _updateBulgePosition(2),
+                ),
+              ],
+            ),
           ),
         ),
       ),
